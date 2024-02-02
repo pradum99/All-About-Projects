e489e0f8bb707de897f0918a8694b4393c7d28aa
Pradum Singha <143691612+pradum99@users.noreply.github.com>
2024-02-02 18:03:07 +0530

    Calculetor

---
+++ BEGINNER/Calculetor/script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+const buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map((button) => {
+    button.addEventListener("click", (e) => {
+        switch (e.target.innerText) {
+            case "C":
+                display.innerText = "";
+                break;
+            case "←":
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            case "=":
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "ERROR!";
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
